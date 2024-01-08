ecaf75637d89d6426f52139cd94f0ffdfe0b219f
Schenier Lopez <schnierlopez@gmail.com>
2024-01-08 23:41:51 +0100

    [Client] Config backend proxy

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react()],
+    base: './',
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true
+            }
+        }
+    }
 })
