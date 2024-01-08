29cb5c16485d5dec66a1dd78f20f80e2129355e3
Schenier Lopez <schnierlopez@gmail.com>
2024-01-09 00:15:27 +0100

    [API] Add logs middleware

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PlayerModule } from './player/player.module';
-import { RedisModule } from './redis/redis.module';
-import { GameModule } from './game/game.module';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {PlayerModule} from './player/player.module';
+import {RedisModule} from './redis/redis.module';
+import {GameModule} from './game/game.module';
+import {RequestTraceMiddleware} from "./middleware/requestTraceMiddleware";
 
 @Module({
-  imports: [RedisModule, PlayerModule, GameModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [RedisModule, PlayerModule, GameModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer): void {
+        consumer.apply(RequestTraceMiddleware).forRoutes('*');
+    }
+}
