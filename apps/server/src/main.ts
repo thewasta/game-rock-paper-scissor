f40e51520ff931c8acf16085f6f2fef6a7e2da78
Schenier Lopez <schnierlopez@gmail.com>
2024-01-21 15:11:49 +0100

    Server port

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
